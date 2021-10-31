8e657565fffa98145b23fb5c6a2b2f795a3c5a4f
ANKUR <ankur02srma@gmail.com>
2021-11-01 00:07:22 +0530

    sets complete

--- sets/01_sets.js
+++ sets/01_sets.js
@@ -32,4 +32,72 @@ function mySet() {
       }
       return false;
   }
+
+  // this method will return the size of the collection
+  this.size = function () {
+      return collection.length;
+  }
+
+  // this method will return the union of two sets
+  // combine two sets and leave out dublicate 
+  this.union = function (otherSet) {
+      var unionSet = new mySet();
+      var firstSet = this.values();
+      var secondSet = otherSet.values();
+      firstSet.forEach(e => {
+          unionSet.add(e);
+      })
+      secondSet.forEach(e => {
+          unionSet.add(e);
+      })
+      return unionSet;
+    }
+
+    // this method will return the intersection of two sets as a new sets
+    // it will return all the item that are available in both sets
+    this.intersection = function (otherSet) {
+        var intersectionSet = new mySet();
+        var firstSet = this.values
+        firstSet.forEach(e => {
+            if(otherSet.has(e)) {
+                intersectionSet.add(e);
+            }
+            return intersectionSet;
+        })
+    }
+
+    // this will return the difference of two sets as a new sets
+    this.difference = function(otherSet) {
+        var differenceSet = new mySet()
+        var firstSet = this.values
+        firstSet.forEach(e => {
+            if(!otherSet.has(e)) {
+                differenceSet.add(e);
+            }
+            return differenceSet
+        })
+    }
+
+    // this method will test if the set is a subset of a difference set
+    this.subset = function(otherSet) {
+        var firstSet = this.values()
+        return firstSet.every(value => {
+            return otherSet.has(value)
+        })
+    }
 }
+
+var setA = new mySet();
+var setB = new mySet();
+setA.add('a')
+setB.add('b')
+setB.add('c')
+setB.add('a')
+setB.add('d')
+setB.add('d')
+setB.add('d')
+setB.add('d')
+setB.add('d')
+
+console.log(setA.subset(setB));
+console.log(setA.intersection(setB).values());
